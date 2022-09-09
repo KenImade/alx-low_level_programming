18401db828d28d3490dcca3e5289b938ac5b0110
Kenneth Imade <kenneth.imade@yahoo.com>
2022-09-09 15:18:59 +0100

    fixed code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,21 +9,24 @@
  */
 int main(void)
 {
-	int num;
+	int num1 num2;
 
-	for (num = 0; num < 90; num++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		for ()
+		for (num2 = num1 +1; num2 < 10; num2++)
 		{
-			putchar((num / 10) + '0');
-			putchar((num % 10) + '0');
-		}
-		if (num != 89)
-		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 9 && num2 == 9)
+				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
