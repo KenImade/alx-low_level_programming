d74875caf42368369c4221abb23e2d5f90d5768c
Kenneth Imade <kenneth.imade@yahoo.com>
2022-09-09 13:03:32 +0100

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int limit;
+
+	limit = 122;
+
+	for (i = 97; i <= limit; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
