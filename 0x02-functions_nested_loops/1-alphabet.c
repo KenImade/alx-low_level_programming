1d3d3912842bc6ef61bb5a9747421eea20e91bab
Kenneth Imade <kenneth.imade@yahoo.com>
2022-09-13 20:51:01 +0100

    removed s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * prints_alphabet - a function that prints the alphabet, in lowercase
+ * print_alphabet - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0
  */
