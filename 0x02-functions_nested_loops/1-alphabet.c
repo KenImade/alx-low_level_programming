e623d5f9517aa44ee6965dbc0acbb66d86f8556b
Kenneth Imade <kenneth.imade@yahoo.com>
2022-09-13 20:48:25 +0100

    removed main function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * prints_alphabet - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-void print_alphabet(void)
 {
 	int n;
 
