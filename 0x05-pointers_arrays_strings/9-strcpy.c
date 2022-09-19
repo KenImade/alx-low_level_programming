aa9ecac8418d94a799607d74d847ccd3212ad0fe
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-09-20 00:45:47 +0100

    replaced - with :

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
  * *_strcpy - copies string pointed to by src to buffer pointed to by dest
  * @dest: the pointer to a string
  * @src: the pointer to a buffer
- * Return - the value of the pointer to dest
+ * Return: the value of the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
