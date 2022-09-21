5f5a8df488e00ddfcc77581bedb14b968dcd36f6
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-09-21 21:02:38 +0100

    fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	if (dest == NULL)
-	{
-		return (NULL);
-	}
-
 	char *ptr = dest;
 
 	while (*src && n--)
@@ -28,3 +23,4 @@ char *_strncpy(char *dest, char *src, int n)
 	*dest = '\0';
 
 	return (ptr);
+}
