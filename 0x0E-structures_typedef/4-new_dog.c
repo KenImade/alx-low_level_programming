979f66083b08d189ce86eaaf6c104fb5b26cce7c
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-10-10 14:49:34 +0100

    added copy functioni

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,38 @@
 #include "dog.h"
 
 /**
+ * _strdup - returns a pointer to space in memory containing copy of string.
+ * @s: pointer to the string
+ * Return: Pointer to a string stored in memory.
+ */
+char *_strdup(char *s)
+{
+	char *dup;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (s == NULL)
+		return (NULL);
+
+	/* Get the length of the string. */
+	while (s[i] != '\0')
+		i += 1;
+
+	/* +1 to include the terminating character in size. */
+	i++;
+
+	dup = malloc(i * sizeof(*dup));
+	if (dup == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		dup[j] = s[j];
+		j++;
+	}
+	return (dup);
+}
+
+/**
  * *new_dog - creates a new dog
  *
  * @name: name of the dog
@@ -17,9 +49,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+	dog->name = _strdup(name);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 	dog->age = age;
 	dog->owner = owner;
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
 
 	return (dog);
 }
