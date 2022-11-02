8e145f7aa61dbad20034b2c17ca8e78d14c537db
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-11-02 12:27:18 +0000

    initial setup

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fontl.h>
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
