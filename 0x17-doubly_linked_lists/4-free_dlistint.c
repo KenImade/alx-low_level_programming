c9307b8b3a05c2d41a7dafb8204961d1d89fad1d
Kenneth Imade <Kenneth.imade@yahoo.com>
2022-12-08 22:36:59 +0000

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_dlistint - free a doubly linked list
+ *
+ * @head: first node in list
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	dlistint_t *next;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
