db58646377fbb45148ddee7b26a300e0285d1fec
Kenneth Imade <Kenneth.imade@yahoo.com>
2023-08-03 23:30:59 +0100

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of
+ * a number
+ *
+ * @n: number
+ *
+ * Return: returns 0
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n / 2);
+	}
+	printf("%d", n % 2);
+}
